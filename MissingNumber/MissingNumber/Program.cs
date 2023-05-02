851acee2f464c88b796d020aeb035cb113649838
Akshaykrishna4216 <129931476+Akshaykrishna4216@users.noreply.github.com>
2023-05-02 16:44:13 +0530

    Add files via upload

---
+++ MissingNumber/MissingNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1=0, s2=0;
+            Console.WriteLine("enter size of array");
+            int n=int.Parse(Console.ReadLine());
+            int[] A = new int[n];
+            Console.WriteLine("enter n-1 elements of array");
+            for(int i = 0; i < n-1; i++)
+            {
+                A[i] =int.Parse(Console.ReadLine());
+            }
+            for(int i=0; i < n; i++)
+            {
+                s1 += A[i];
+            }
+            for(int i=0 ; i <= n ; i++)
+            {
+                s2 += i;
+            }
+            Console.WriteLine("missing Element:" + (s2 - s1));
+        }
+    }
+}
---
+++ subarray2/subarray2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Globalization;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace subarray
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter the num of elements");
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Enter the value");
+
+            int val = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                // Console.WriteLine(i+"i");
+
+                int j = i + 1;
+
+                sum = arr[i];
+
+                while (j < n)
+
+                {
+
+                    sum += arr[j];
+
+                    // Console.WriteLine(j+"j");
+
+                    // Console.WriteLine(sum);
+
+                    if (sum == val)
+
+                    {
+
+                        i += 1;
+
+                        j += 1;
+
+                        Console.WriteLine(i + " " + j);
+
+                        n = 0;
+
+                    }
+
+                    j++;
+
+                }
+
+                j = i + 1;
+
+                sum = 0;
+
+            }
+
+        }
+
+    }
+
+}
